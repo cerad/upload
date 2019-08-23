4ca2f7bc8560f5f4a1973b9dcacb3d32c837d218
Arthur Hundiak <ahundiak@gmail.com>
2019-08-23 08:02:32 -0500

    Basic upload example

--- src/Type/SlideshowBackgroundType.php
+++ src/Type/SlideshowBackgroundType.php
@@ -24,7 +24,7 @@ class SlideshowBackgroundType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            //'data_class' => SlideshowBackground::class,
+            'data_class' => SlideshowBackground::class,
         ]);
     }
 }
